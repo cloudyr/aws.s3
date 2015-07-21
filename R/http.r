303bbc6f31feb44d0b8a4174fad51dc055558360
almartin82 <almartin@gmail.com>
2015-07-20 23:28:44 -0400

    roxygenized doc for s3HTTP, resolves Codoc mismatches from documentation object s3HTTP from CMD check

--- R/http.r
+++ R/http.r
@@ -1,3 +1,32 @@
+#' @title S3 HTTP Requests
+#' 
+#' @description This is the workhorse function for executing API requests for S3.
+#'
+#' @details This is mostly an internal function for executing API requests. 
+#' In almost all cases, users do not need to access this directly.
+#' 
+#' @param verb A character string containing an HTTP verb, defaulting to \dQuote{GET}.
+#' @param url A character string containing the URL for the API endpoint.
+#' @param bucket A character string containing the name of AWS bucket to use.
+#' @param path A character string with the name of the object to put in the bucket 
+#' (sometimes called the object or 'key name' in the AWS documentation.)
+#' @param headers  
+#' @param region A character string containing the AWS region.
+#' If missing, defaults to \dQuote{us-east-1}.
+#' @param key A character string containing an AWS Access Key ID. 
+#' If missing, defaults to value stored in environment variable \dQuote{AWS_ACCESS_KEY_ID}.
+#' @param secret A character string containing an AWS Secret Access Key. 
+#' If missing, defaults to value stored in environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
+#' @param parse_response 
+#' @param ... Additional arguments passed to an HTTP request function, 
+#' such as \code{\link[httr]{GET}}.
+#'
+#' @author Thomas J. Leeper \email{thosjleeper@@gmail.com}
+#' 
+#' @return the S3 response, or the relevant error.
+#' 
+#' @export
+
 s3HTTP <- function(verb = "GET",
                    url = "https://s3.amazonaws.com",
                    bucket = "", 
@@ -10,7 +39,7 @@ s3HTTP <- function(verb = "GET",
                    ...) {
     
     ## Endpoint must match region (the default s3.amazonaws.com is us-east-1 region only)
-    if(region != "us-east-1" && url == "https://s3.amazonaws.com")
+    if (region != "us-east-1" && url == "https://s3.amazonaws.com")
       url = paste0("https://s3-", region, ".amazonaws.com")
     url <- paste0(url, "/", bucket, path)
     current <- Sys.time()
@@ -18,7 +47,7 @@ s3HTTP <- function(verb = "GET",
     p <- parse_url(url)
     action <- if(p$path == "") "/" else paste0("/",p$path)
     
-    if(key == "") {
+    if (key == "") {
         headers$`x-amz-date` <- d_timestamp
         H <- do.call(add_headers, headers)
     } else {
@@ -39,7 +68,7 @@ s3HTTP <- function(verb = "GET",
         H <- do.call(add_headers, headers)
     }
     
-    if(verb == "GET") {
+    if (verb == "GET") {
         r <- GET(url, H, ...)
     } else if (verb == "HEAD") {
         r <- HEAD(url, H, ...)
@@ -53,18 +82,18 @@ s3HTTP <- function(verb = "GET",
         r <- VERB("OPTIONS", url, H, ...)
     }
     
-    if(parse_response) {
+    if (parse_response) {
         x <- xmlToList(xmlParse(content(r, "text")))
         response <- xmlToList(xmlParse(content))
-        if(http_status(r)$category == "client error") {
+        if (http_status(r)$category == "client error") {
             warn_for_status(r)
             h <- headers(r)
             out <- structure(x, headers = h, class = "aws_error")
         } else {
             out <- response
         }
-        if(inherits(out, "aws_error")) {
-          if(exists("S")) {
+        if (inherits(out, "aws_error")) {
+          if (exists("S")) {
             attr(out, "request_canonical") <- S$CanonicalRequest
             attr(out, "request_string_to_sign") <- S$StringToSign
             attr(out, "request_signature") <- S$SignatureHeader
