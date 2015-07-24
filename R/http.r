a34c734862cb30205578ae7a275af66fd84aa1c2
Thomas J. Leeper <thosjleeper@gmail.com>
2015-07-24 15:21:43 +0200

    modify signing to avoid reading file into memory

--- R/http.r
+++ R/http.r
@@ -56,7 +56,6 @@ s3HTTP <- function(verb = "GET",
         headers$`x-amz-date` <- d_timestamp
         H <- do.call(httr::add_headers, headers)
     } else {
-        r <- if(file.exists(request_body)) readBin(request_body, what = raw(), n = file.size(request_body)) else request_body
         Sig <- aws.signature::signature_v4_auth(
                datetime = d_timestamp,
                region = region,
@@ -66,7 +65,7 @@ s3HTTP <- function(verb = "GET",
                query_args = p$query,
                canonical_headers = list(host = p$hostname,
                                         `x-amz-date` = d_timestamp),
-               request_body = r,
+               request_body = request_body,
                key = key, secret = secret)
         headers$`x-amz-date` <- d_timestamp
         headers$`x-amz-content-sha256` <- Sig$BodyHash
