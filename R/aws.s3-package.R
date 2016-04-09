9a4155c29673e2859bbb3c191593e27154375bd8
leeper <thosjleeper@gmail.com>
2016-04-09 13:00:38 +0100

    roxygenize

---
+++ R/aws.s3-package.R
@@ -0,0 +1,9 @@
+#' @name aws.s3-package
+#' @title aws.s3-package
+#' @aliases aws.s3-package aws.s3
+#' @docType package
+#' @description AWS S3 Client Package
+#' @details A simple client package for the Amazon Web Services (AWS) Simple Storage Service (S3) REST API.
+#' @author Thomas J. Leeper <thosjleeper@gmail.com>
+#' @keywords package 
+NULL
