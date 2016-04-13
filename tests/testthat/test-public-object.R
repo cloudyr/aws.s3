96e47e042e54c03ebf4b32cbbbe0f76bb82ca7ba
Tyler Hunt <tyler.hunt@utah.edu>
2016-04-13 11:16:04 -0600

    modified tests to point to us-east-1

--- tests/testthat/test-public-object.R
+++ tests/testthat/test-public-object.R
@@ -1,7 +1,12 @@
 context("Public object tests")
 
 test_that("basic usage of get_object for anonymous user", {
-  ex <- get_object(object = 'README.analysis_history', bucket = '1000genomes', key="", secret="")
+  ex <- get_object(
+    object = 'README.analysis_history',
+    bucket = '1000genomes',
+    key="",
+    secret="",
+    region = 'us-east-1')
   
   expect_true(is.raw(ex))
 })
