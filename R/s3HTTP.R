d09c06e2f66fe269db024f15a95b7f4abd4cef9b
Thomas J. Leeper <thosjleeper@gmail.com>
2016-11-21 09:53:31 +0000

    experiment with iam sts support (#65)

--- R/s3HTTP.R
+++ R/s3HTTP.R
@@ -1,10 +1,6 @@
 #' @title S3 HTTP Requests
-#' 
 #' @description This is the workhorse function for executing API requests for S3.
-#'
-#' @details This is mostly an internal function for executing API requests. 
-#' In almost all cases, users do not need to access this directly.
-#' 
+#' @details This is mostly an internal function for executing API requests. In almost all cases, users do not need to access this directly.
 #' @param verb A character string containing an HTTP verb, defaulting to \dQuote{GET}.
 #' @param bucket A character string with the name of the bucket, or an object of class \dQuote{s3_bucket}. If the latter and a region can be inferred from the bucket object attributes, then that region is used instead of \code{region}.
 #' @param path A character string with the name of the object to put in the bucket 
@@ -13,19 +9,13 @@
 #' @param headers a list of request headers for the REST call.   
 #' @param request_body character string of request body data.
 #' @param accelerate A logical indicating whether to use AWS transfer acceleration, which can produce significant speed improvements for cross-country transfers. Acceleration only works with buckets that do not have dots in bucket name.
-#' @param region A character string containing the AWS region. Ignored if region can be inferred from \code{bucket}.
-#' If missing, defaults to \dQuote{us-east-1}.
-#' @param key A character string containing an AWS Access Key ID. 
-#' If missing, defaults to value stored in environment variable \dQuote{AWS_ACCESS_KEY_ID}.
-#' @param secret A character string containing an AWS Secret Access Key. 
-#' If missing, defaults to value stored in environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
-#' @param parse_response return the response as is, or parse and return as a list?  
-#' default is TRUE.
-#' @param ... Additional arguments passed to an HTTP request function.
-#' such as \code{\link[httr]{GET}}.
-#'
+#' @param region A character string containing the AWS region. Ignored if region can be inferred from \code{bucket}. If missing, defaults to \dQuote{us-east-1}.
+#' @param key A character string containing an AWS Access Key ID. If missing, defaults to value stored in environment variable \dQuote{AWS_ACCESS_KEY_ID}.
+#' @param secret A character string containing an AWS Secret Access Key. If missing, defaults to value stored in environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
+#' @param session_token Optionally, a character string containing an AWS temporary Session Token. If missing, defaults to value stored in environment variable \dQuote{AWS_SESSION_TOKEN}.
+#' @param parse_response return the response as is, or parse and return as a list? Default is TRUE.
+#' @param ... Additional arguments passed to an HTTP request function. such as \code{\link[httr]{GET}}.
 #' @return the S3 response, or the relevant error.
-#' 
 #' @importFrom httr GET POST PUT HEAD DELETE VERB upload_file parse_url add_headers
 #' @importFrom httr http_error http_status warn_for_status content headers
 #' @importFrom xml2 read_xml as_list
@@ -42,6 +32,7 @@ s3HTTP <- function(verb = "GET",
                    region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
                    key = Sys.getenv("AWS_ACCESS_KEY_ID"), 
                    secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
+                   session_token = Sys.getenv("AWS_SESSION_TOKEN"),
                    parse_response = TRUE, 
                    ...) {
     
@@ -69,13 +60,13 @@ s3HTTP <- function(verb = "GET",
                                 `x-amz-date` = d_timestamp), headers)
     
     if (is.null(query) && !is.null(p$query)) {
-        query <- p$query
+        query <- p[["query"]]
     }
     if (all(sapply(query, is.null))) {
         query <- NULL
     }
     if (key == "") {
-        headers$`x-amz-date` <- d_timestamp
+        headers[["x-amz-date"]] <- d_timestamp
         Sig <- list()
         H <- do.call(add_headers, headers)
     } else {
@@ -88,10 +79,13 @@ s3HTTP <- function(verb = "GET",
                query_args = query,
                canonical_headers = canonical_headers,
                request_body = request_body,
-               key = key, secret = secret)
-        headers$`x-amz-date` <- d_timestamp
-        headers$`x-amz-content-sha256` <- Sig$BodyHash
-        headers$Authorization <- Sig$SignatureHeader
+               key = key, secret = secret, session_token = session_token)
+        headers[["x-amz-date"]] <- d_timestamp
+        headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+        if (!is.null(session_token) && session_token != "") {
+            headers[["x-amz-security-token"]] <- session_token
+        }
+        headers[["Authorization"]] <- Sig[["SignatureHeader"]]
         H <- do.call(add_headers, headers)
     }
     if (verb == "GET") {
