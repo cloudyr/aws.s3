6e31f28850364e78c509ff2f4bedb40eed714407
Thomas J. Leeper <thosjleeper@gmail.com>
2017-01-15 11:26:16 +0000

    add doc ex of passing object to put_object() (#100)

--- R/get_object.R
+++ R/get_object.R
@@ -7,35 +7,39 @@
 #' @param headers List of request headers for the REST call.
 #' @param parse_response Passed through to \code{\link{s3HTTP}}, as this function requires a non-default setting. There is probably no reason to ever change this.
 #' @template dots
-#' @details \code{get_object} retrieves an object into memory. \code{save_object} saves an object to a local file. \code{head_object} checks whether an object exists by executing an HTTP HEAD request; this can be useful for checking object headers such as \dQuote{content-length} or \dQuote{content-type}.
+#' @details \code{get_object} retrieves an object into memory as a raw vector. \code{save_object} saves an object to a local file. \code{head_object} checks whether an object exists by executing an HTTP HEAD request; this can be useful for checking object headers such as \dQuote{content-length} or \dQuote{content-type}.
+#'
+#' Some users may find the raw vector response format of \code{get_object} unfamiliar. The object will also carry attributes, including \dQuote{content-type}, which may be useful for deciding how to subsequently process the vector. Two common strategies are as follows. For text content types, running \code{\link[base]{charToRaw}} may be the most useful first step to make the response human-readable. Alternatively, converting the raw vector into a connection using \code{\link[base]{rawConnection}} may also be useful, as that can often then be passed to parsing functions just like a file connection would be.
+#' 
 #' @examples
 #' \dontrun{
-#' # get an object in memory
-#' ## create bucket
-#' b <- put_bucket("myexamplebucket")
-#' ## save a dataset to the bucket
-#' s3save(mtcars, bucket = b, object = "mtcars")
-#' obj <- get_bucket(b)
-#' ## get the object in memory
-#' x <- get_object(obj[[1]])
-#' load(rawConnection(x))
-#' "mtcars" %in% ls()
+#'   # get an object in memory
+#'   ## create bucket
+#'   b <- put_bucket("myexamplebucket")
+#'   
+#'   ## save a dataset to the bucket
+#'   s3save(mtcars, bucket = b, object = "mtcars")
+#'   obj <- get_bucket(b)
+#'   ## get the object in memory
+#'   x <- get_object(obj[[1]])
+#'   load(rawConnection(x))
+#'   "mtcars" %in% ls()
 #'
-#' # save an object locally
-#' y <- get_object(obj[[1]], file = object[[1]][["Key"]])
-#' y %in% dir()
+#'   # save an object locally
+#'   y <- get_object(obj[[1]], file = object[[1]][["Key"]])
+#'   y %in% dir()
 #' 
-#' # return object using s3:// syntax
-#' get_object("s3://myexamplebucket/mtcars")
+#'   # return object using 'S3 URI' syntax
+#'   get_object("s3://myexamplebucket/mtcars")
 #' 
-#' # return parts of an object
-#' ## use 'Range' header to specify bytes
-#' get_object(object = obj[[1]], headers = list('Range' = 'bytes=1-120'))
+#'   # return parts of an object
+#'   ## use 'Range' header to specify bytes
+#'   get_object(object = obj[[1]], headers = list('Range' = 'bytes=1-120'))
 #' }
 #' @return If \code{file = NULL}, a raw object. Otherwise, a character string containing the file name that the object is saved to.
 #' @references \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectGET.html}{API Documentation: GET Object}
 #' @references \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectHEAD.html}{API Document: HEAD Object}
-#' @seealso \code{\link{get_bucket}}, \code{\link{s3curl}}
+#' @seealso \code{\link{get_bucket}}, \code{\link{put_object}}, \code{\link{delete_object}}, \code{\link{s3curl}}
 #' @export
 get_object <- function(object, bucket, headers = list(), parse_response = FALSE, ...) {
     if (missing(bucket)) {
--- R/put_object.R
+++ R/put_object.R
@@ -1,19 +1,40 @@
 #' @title Put object
 #' @description Puts an object into an S3 bucket
-#'
-#' @param file A character string containing the filename (or full path) of the file you want to upload to S3.
+#' @param file A character string containing the filename (or full path) of the file you want to upload to S3. Alternatively, an raw vector containing the file can be passed directly, in which case \code{object} needs to be specified explicitly.
 #' @template bucket
-#' @param object A character string containing the name the object should 
-#' have in S3 (i.e., its "object key"). If missing, the filename is used.
+#' @param object A character string containing the name the object should have in S3 (i.e., its "object key"). If missing, the filename is used.
 #' @template dots
 #' @param headers List of request headers for the REST call.   
-#'
-#' @return If successful, \code{TRUE}, otherwise an aws_error object.
+#' @details This provide a generic interface for sending files (or serialized, in-memory representations thereof) to S3. Some convenience wrappers are provided for common tasks: \code{\link{s3save}} and \code{\link{s3saveRDS}}.
+#' 
+#' @return If successful, \code{TRUE}, otherwise an \dQuote{aws_error} object.
+#' @examples
+#' \dontrun{
+#'   library("datasets")
+#' 
+#'   # write file to S3
+#'   tmp <- tempfile()
+#'   on.exit(unlink(tmp))
+#'   utils::write.csv(mtcars, file = tmp)
+#'   put_object(tmp, object = "mtcars.csv", bucket = "myexamplebucket")
+#' 
+#'   # write serialized, in-memory object to S3
+#'   x <- rawConnection(raw(0), "w")
+#'   utils::write.csv(mtcars, x)
+#'   put_object(rawConnectionValue(x), object = "mtcars.csv", bucket = "myexamplebucketname")
+#' 
+#'   # alternative "S3 URI" syntax:
+#'   put_object(rawConnectionValue(x), object = "s3://myexamplebucketname/mtcars.csv")
+#'   close(x)
+#' 
+#'   # read the object back from S3
+#'   read.csv(text = rawToChar(get_object(object = "s3://myexamplebucketname/mtcars.csv")))
+#' }
 #' @references \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTObjectPUT.html}{API Documentation}
-#' @seealso \code{\link{delete_object}}
+#' @seealso \code{\link{put_bucket}}, \code{\link{get_object}}, \code{\link{delete_object}}
 #' @export
 put_object <- function(file, object, bucket, headers = list(), ...) {
-    if (missing(object)) {
+    if (missing(object) && is.character(file)) {
         object <- basename(file)
     } else {
         if (missing(bucket)) {
