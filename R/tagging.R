7e55340fa067fd88d984e451e4f315ae4b7a75aa
Thomas J. Leeper <thosjleeper@gmail.com>
2017-01-21 15:12:17 +0000

    fix doc error for R CMD check

--- R/tagging.R
+++ R/tagging.R
@@ -1,15 +1,13 @@
 #' @rdname tagging
 #' @title Bucket tagging
 #' @description Get/delete the tag set for a bucket.
-#'
 #' @template bucket
+#' @param tags A list containing key-value pairs of tag names and values.
 #' @template dots
-#'
-#' @return A list containing the tag set, if one has been set.
-#' For \code{delete_tagging}: \code{TRUE} if successful, \code{FALSE} otherwise. An \code{aws_error} object may be returned if the request failed.
+#' @return A list containing the tag set, if one has been set. For \code{delete_tagging}: \code{TRUE} if successful, \code{FALSE} otherwise. An \code{aws_error} object may be returned if the request failed.
 #' @references
-#' \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGETtagging.html}{API Documentation}
-#' \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketDELETEtagging.html}{API Documentation}
+#'  \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketGETtagging.html}{API Documentation}
+#'  \href{http://docs.aws.amazon.com/AmazonS3/latest/API/RESTBucketDELETEtagging.html}{API Documentation}
 #' @export
 get_tagging <- function(bucket, ...){
     r <- s3HTTP(verb = "GET", 
