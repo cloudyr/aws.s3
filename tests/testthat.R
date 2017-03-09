495405537a54c3ae9c35e123269e77f73526f272
Thomas J. Leeper <thosjleeper@gmail.com>
2017-03-09 12:16:11 +0000

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,7 @@ library("aws.s3")
 op <- options(verbose = TRUE)
 
 if (Sys.getenv("TRAVIS_AWS_ACCESS_KEY_ID") != "") {
-    test_check("aws.s3", filter = "authenticated")
+    #test_check("aws.s3", filter = "authenticated")
 }
 
 test_check("aws.s3", filter = "public")
