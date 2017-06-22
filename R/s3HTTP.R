cloudyr/aws.s3

commit 9627d8fe415ab9b293a02256a7e1e7645bd40052

upgrade to aws.signature (>=0.3.2)

DESCRIPTION
@@ -1,8 +1,8 @@
 Package: aws.s3
 Type: Package
 Title: AWS S3 Client Package
-Version: 0.3.3
-Date: 2017-05-24
+Version: 0.3.4
+Date: 2017-06-21
 Authors@R: c(person("Thomas J.", "Leeper", role = c("aut", "cre"),
                     email = "thosjleeper@gmail.com"),
              person("Boettiger", "Carl", role = "ctb"),
@@ -24,7 +24,7 @@ Imports:
     xml2 (> 1.0.0),
     base64enc,
     digest,
-    aws.signature (>= 0.2.6)
+    aws.signature (>= 0.3.2)
 Suggests:
     testthat,
     datasets

R/s3HTTP.R
@@ -40,10 +40,10 @@ function(verb = "GET",
          url_style = c("path", "virtual"),
          base_url = "s3.amazonaws.com",
          verbose = getOption("verbose", FALSE),
-         region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
-         key = Sys.getenv("AWS_ACCESS_KEY_ID"), 
-         secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
-         session_token = Sys.getenv("AWS_SESSION_TOKEN"),
+         region = NULL, 
+         key = NULL, 
+         secret = NULL, 
+         session_token = NULL,
          ...) {
     
     bucketname <- get_bucketname(bucket)
@@ -218,8 +218,8 @@ function(bucketname,
             url_style <- "virtual"
         }
         # handle region
-        if (region %in% c("us-east-1", "")) {
-            if (region == "") {
+        if (is.null(region) || region %in% c("us-east-1", "")) {
+            if (is.null(region) || region == "") {
                 if (isTRUE(verbose)) {
                     message("Option 'region' is missing, so 'us-east-1' assumed.")
                 }

README.Rmd
@@ -2,20 +2,24 @@
 
 **aws.s3** is a simple client package for the Amazon Web Services (AWS) Simple Storage Service (S3) REST API. While [other packages](https://github.com/ropensci/webservices#amazon) currently connect R to S3, they do so incompletely (mapping only some of the API endpoints to R) and most implementations rely on the AWS command-line tools, which users may not have installed on their system.
 
-To use the package, you will need an AWS account and enter your credentials into R. Your keypair can be generated on the [IAM Management Console](https://aws.amazon.com/) under the heading *Access Keys*. Note that you only have access to your secret key once. After it is generated, you need to save it in a secure location. New keypairs can be generated at any time if yours has been lost, stolen, or forgotten. 
+To use the package, you will need an AWS account and to enter your credentials into R. Your keypair can be generated on the [IAM Management Console](https://aws.amazon.com/) under the heading *Access Keys*. Note that you only have access to your secret key once. After it is generated, you need to save it in a secure location. New keypairs can be generated at any time if yours has been lost, stolen, or forgotten. The [**aws.iam** package](https://github.com/cloudyr/aws.iam) profiles tools for working with IAM, including creating roles, users, groups, and credentials programmatically; it is not needed to *use* IAM credentials.
 
-By default, all **cloudyr** packages look for the access key ID and secret access key in environment variables. You can also use this to specify a default region or a temporary "session token". For example:
+By default, all **cloudyr** packages for AWS services allow the use of credentials specified in a number of ways, beginning with:
 
-```R
-Sys.setenv("AWS_ACCESS_KEY_ID" = "mykey",
-           "AWS_SECRET_ACCESS_KEY" = "mysecretkey",
-           "AWS_DEFAULT_REGION" = "us-east-1",
-           "AWS_SESSION_TOKEN" = "mytoken")
-```
+ 1. User-supplied values passed directly to functions.
+ 2. Environment variables, which can alternatively be set on the command line prior to starting R or via an `Renviron.site` or `.Renviron` file, which are used to set environment variables in R during startup (see `? Startup`). Or they can be set within R:
+ 
+    ```R
+    Sys.setenv("AWS_ACCESS_KEY_ID" = "mykey",
+               "AWS_SECRET_ACCESS_KEY" = "mysecretkey",
+               "AWS_DEFAULT_REGION" = "us-east-1",
+               "AWS_SESSION_TOKEN" = "mytoken")
+    ```
+ 3. If R is running an EC2 instance, the role profile credentials provided by [**aws.ec2metadata**](https://cran.r-project.org/package=aws.ec2metadata).
+ 4. Profiles saved in a `/.aws/credentials` "dot file" in the current working directory. The `"default" profile is assumed if none is specified.
+ 5. [A centralized `~/.aws/credentials` file](https://blogs.aws.amazon.com/security/post/Tx3D6U6WSFGOK2H/A-New-and-Standardized-Way-to-Manage-Credentials-in-the-AWS-SDKs), containing credentials for multiple accounts. The `"default" profile is assumed if none is specified.
 
-These can alternatively be set on the command line prior to starting R or via an `Renviron.site` or `.Renviron` file, which are used to set environment variables in R during startup (see `? Startup`).
-
-If you work with multiple AWS accounts, another option that is consistent with other Amazon SDKs is to create [a centralized `~/.aws/credentials` file](https://blogs.aws.amazon.com/security/post/Tx3D6U6WSFGOK2H/A-New-and-Standardized-Way-to-Manage-Credentials-in-the-AWS-SDKs), containing credentials for multiple accounts. You can then use credentials from this file on-the-fly by simply doing:
+Profiles stored locally or in a centralized location (e.g., `~/.aws/credentials`) can also be invoked via:
 
 ```R
 # use your 'default' account credentials

man/s3HTTP.Rd
@@ -8,11 +8,8 @@ s3HTTP(verb = "GET", bucket = "", path = "", query = NULL,
   headers = list(), request_body = "", accelerate = FALSE,
   dualstack = FALSE, parse_response = TRUE, check_region = TRUE,
   url_style = c("path", "virtual"), base_url = "s3.amazonaws.com",
-  verbose = getOption("verbose", FALSE),
-  region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"),
-  key = Sys.getenv("AWS_ACCESS_KEY_ID"),
-  secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
-  session_token = Sys.getenv("AWS_SESSION_TOKEN"), ...)
+  verbose = getOption("verbose", FALSE), region = NULL, key = NULL,
+  secret = NULL, session_token = NULL, ...)
 }
 \arguments{
 \item{verb}{A character string containing an HTTP verb, defaulting to \dQuote{GET}.}

commit 94512ffc4595e3d1fe506699b07720838a1647ac

fix bug introduced in 9627d8fe415ab9b293a02256a7e1e7645bd40052 (#142)

DESCRIPTION
@@ -1,8 +1,8 @@
 Package: aws.s3
 Type: Package
 Title: AWS S3 Client Package
-Version: 0.3.4
-Date: 2017-06-21
+Version: 0.3.5
+Date: 2017-06-22
 Authors@R: c(person("Thomas J.", "Leeper", role = c("aut", "cre"),
                     email = "thosjleeper@gmail.com"),
              person("Boettiger", "Carl", role = "ctb"),

R/s3HTTP.R
@@ -46,6 +46,14 @@ function(verb = "GET",
          session_token = NULL,
          ...) {
     
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # validate bucket name and region
     bucketname <- get_bucketname(bucket)
     if (isTRUE(check_region) && (bucketname != "")) {
         if (isTRUE(verbose)) {
@@ -60,23 +68,25 @@ function(verb = "GET",
         }
     }
     
+    # validate arguments and setup request URL
+    current <- Sys.time()
+    d_timestamp <- format(current, "%Y%m%dT%H%M%SZ", tz = "UTC")
+    
     url_style <- match.arg(url_style)
     url <- setup_s3_url(bucketname, region, path, accelerate, url_style = url_style, base_url = base_url, verbose = verbose)
     p <- parse_url(url)
-    
-    current <- Sys.time()
-    d_timestamp <- format(current, "%Y%m%dT%H%M%SZ", tz = "UTC")
     action <- if (p$path == "") "/" else paste0("/", p$path)
     canonical_headers <- c(list(host = p$hostname,
                                 `x-amz-date` = d_timestamp), headers)
-
     if (is.null(query) && !is.null(p$query)) {
         query <- p[["query"]]
     }
     if (all(sapply(query, is.null))) {
         query <- NULL
     }
-    if (key == "") {
+    
+    # assess whether request is authenticated or not
+    if (is.null(key) || key == "") {
         if (isTRUE(verbose)) {
             message("Executing request without AWS credentials")
         }
@@ -107,6 +117,8 @@ function(verb = "GET",
         headers[["Authorization"]] <- Sig[["SignatureHeader"]]
         H <- do.call(add_headers, headers)
     }
+    
+    # execute request
     if (verb == "GET") {
         r <- GET(url, H, query = query, ...)
     } else if (verb == "HEAD") {
@@ -149,6 +161,7 @@ function(verb = "GET",
         r <- VERB("OPTIONS", url, H, query = query, ...)
     }
     
+    # handle response, failing if HTTP error occurs
     if (isTRUE(parse_response)) {
         out <- parse_aws_s3_response(r, Sig, verbose = verbose)
     } else {