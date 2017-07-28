cloudyr/aws.s3

commit 12332edbdddff484b9f7c999fe31e2baf43d5679

switch rawConnection() to tempfile()

R/s3source.R
@@ -37,7 +37,7 @@ s3source <- function(object, bucket, ..., opts = NULL) {
     } else {
         r <- do.call("get_object", c(list(bucket = bucket, object = object), opts))
     }
-    tmp <- rawConnection(r, "r+")
-    on.exit(close(tmp))
+    tmp <- tmpfile(fileext = ".rdata")
+    on.exit(unlink(tmp))
     return(source(tmp, ...))
 }

DESCRIPTION
@@ -1,8 +1,8 @@
 Package: aws.s3
 Type: Package
 Title: AWS S3 Client Package
-Version: 0.2.3
-Date: 2017-04-21
+Version: 0.2.4
+Date: 2017-04-22
 Authors@R: c(person("Thomas J.", "Leeper", role = c("aut", "cre"),
                     email = "thosjleeper@gmail.com"),
              person("Boettiger", "Carl", role = "ctb"),

R/s3save.R
@@ -31,17 +31,17 @@
 #' @seealso \code{\link{s3saveRDS}},\code{\link{s3readRDS}}
 #' @export
 s3save <- function(..., object, bucket, envir = parent.frame(), opts = NULL) {
-    tmp <- rawConnection(raw(0), "r+")
-    on.exit(close(tmp))
+    tmp <- tmpfile(fileext = ".rdata")
+    on.exit(unlink(tmp))
     save(..., file = tmp, envir = envir)
     if (missing(bucket)) {
         bucket <- get_bucketname(object)
     }
     object <- get_objectkey(object)
     if (is.null(opts)) {
-        r <- put_object(file = rawConnectionValue(tmp), bucket = bucket, object = object)
+        r <- put_object(file = tmp, bucket = bucket, object = object)
     } else {
-        r <- do.call("put_object", c(list(file = rawConnectionValue(tmp), bucket = bucket, object = object), opts))
+        r <- do.call("put_object", c(list(file = tmp, bucket = bucket, object = object), opts))
     }
     return(invisible(r))
 }
@@ -49,17 +49,17 @@ s3save <- function(..., object, bucket, envir = parent.frame(), opts = NULL) {
 #' @rdname s3save
 #' @export
 s3save_image <- function(object, bucket, opts = NULL) {
-    tmp <- rawConnection(raw(0), "r+")
-    on.exit(close(tmp))
+    tmp <- tmpfile(fileext = ".rdata")
+    on.exit(unlink(tmp))
     save.image(file = tmp)
     if (missing(bucket)) {
         bucket <- get_bucketname(object)
     }
     object <- get_objectkey(object)
     if (is.null(opts)) {
-        r <- put_object(file = rawConnectionValue(tmp), bucket = bucket, object = object)
+        r <- put_object(file = tmp, bucket = bucket, object = object)
     } else {
-        r <- do.call("put_object", c(list(file = rawConnectionValue(tmp), bucket = bucket, object = object), opts))
+        r <- do.call("put_object", c(list(file = tmp, bucket = bucket, object = object), opts))
     }
     return(invisible(r))
 }
@@ -71,9 +71,9 @@ s3load <- function(object, bucket, envir = parent.frame(), ...) {
         bucket <- get_bucketname(object)
     }
     object <- get_objectkey(object)
-    r <- get_object(bucket = bucket, object = object, parse_response = FALSE, ...)
-    tmp <- rawConnection(r, "r")
-    on.exit(close(tmp))
+    tmp <- tmpfile(fileext = ".rdata")
+    on.exit(unlink(tmp))
+    r <- save_object(bucket = bucket, object = object, file = tmp, ...)
     load(tmp, envir = envir)
     return(invisible())
 }

R/s3saveRDS.R
@@ -43,10 +43,8 @@ s3readRDS <- function(bucket, object, ...) {
         bucket <- get_bucketname(object)
     }
     object <- get_objectkey(object)
-    r <- get_object(bucket = bucket, object = object, ...)
-    if (typeof(r) == 'raw') {
-        return(unserialize(memDecompress(from = as.vector(r), type = 'gzip')))
-    } else {
-        return(r)
-    }
+    tmp <- tmpfile(fileext = ".rdata")
+    on.exit(unlink(tmp))
+    r <- save_object(bucket = bucket, object = object, file = tmp, ...)
+    readRDS(tmp)
 }

commit 009acfb63f1613e6f2a9e6db5c2825dc9f746157

s3source now sourcing the S3 content rather than an empty temp file.
Fixing bug introduced by commit 12332edbdddff484b9f7c999fe31e2baf43d5679

R/s3source.R
@@ -39,5 +39,6 @@ s3source <- function(object, bucket, ..., opts = NULL) {
     }
     tmp <- tempfile(fileext = ".R")
     on.exit(unlink(tmp))
+    writeBin(object = r, con = tmp)
     return(source(tmp, ...))
 }