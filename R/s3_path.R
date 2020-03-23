ee42d61ffb26e7f00789f293a47bf2623a8e540d
Jose <jsameijeiras@gmail.com>
2020-03-23 12:28:09 +0100

    Added s3 path treatment function

---
+++ R/s3_path.R
@@ -0,0 +1,20 @@
+#' @rdname s3_path
+#' @title Treatment of the s3 path.
+#' @description Use S3 path to return object and bucket without needing further coding.
+#' @param path For \code{s3_path}, a single path that would be decompose into object and bucket.
+#' @return For \code{s3_path}, a vector with two variables, first one is bucket second one is object.
+
+
+@examples
+#' \dontrun{
+#' s3read_using(read_delim, delim = ";", object = s3_path(path)[2],bucket = s3_path(path[1]))
+#'}
+
+read_s3_path <- function(s3_path) {
+  
+  bucket <- str_match(s3_path, "s3://([^/]*)/")[2]
+  object <- str_match(s3_path, paste0(bucket,"/(.*)"))[2]
+  
+  return(c(bucket,object))
+  
+}
