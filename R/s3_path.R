d596ed6e09947ab547b0f207e4231e1995a0070b
Jose <jsameijeiras@gmail.com>
2020-03-23 12:46:42 +0100

    Added documentation

--- R/s3_path.R
+++ R/s3_path.R
@@ -5,15 +5,15 @@
 #' @return For \code{s3_path}, a vector with two variables, first one is bucket second one is object.
 
 
-@examples
+#' @examples
 #' \dontrun{
 #' s3read_using(read_delim, delim = ";", object = s3_path(path)[2],bucket = s3_path(path[1]))
 #'}
 
 read_s3_path <- function(s3_path) {
   
-  bucket <- str_match(s3_path, "s3://([^/]*)/")[2]
-  object <- str_match(s3_path, paste0(bucket,"/(.*)"))[2]
+  bucket <- stringr::str_match(s3_path, "s3://([^/]*)/")[2]
+  object <- stringr::str_match(s3_path, paste0(bucket,"/(.*)"))[2]
   
   return(c(bucket,object))
   
